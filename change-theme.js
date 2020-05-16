3908dc7dde679438a9a6049c1eae1f8a17f0d753
Glitch (lunation) <none>
2020-05-16 11:56:53 +0000

    fixed new moon picture and added page loader

--- change-theme.js
+++ change-theme.js
@@ -3,31 +3,22 @@
  */
 
 // change logo:
-var logos = {
+const logos = {
   dark: "https://cdn.glitch.com/e3f08c41-d9c5-46da-95b8-60d91e4499eb%2Flogo_light.png?v=1589275742566",
   light:"https://cdn.glitch.com/e3f08c41-d9c5-46da-95b8-60d91e4499eb%2Flogo_dark.png?v=1589488201424",
   warm: "https://cdn.glitch.com/e3f08c41-d9c5-46da-95b8-60d91e4499eb%2Flogo_warm.png?v=1589275742674"
 };
 
-// new moon images
-var newmoon = {
-  dark: "https://cdn.glitch.com/e3f08c41-d9c5-46da-95b8-60d91e4499eb%2F00_New_moon_light.png?v=1589406891557",
-  light: "https://cdn.glitch.com/e3f08c41-d9c5-46da-95b8-60d91e4499eb%2F00_New_moon_dark.png?v=1589406891618",
-  warm: "https://cdn.glitch.com/e3f08c41-d9c5-46da-95b8-60d91e4499eb%2F00_New_moon_warm.png?v=1589406891584"
-};
-
 // load theme from localStorage
 var lastPick = localStorage.getItem("themePick");
-window.onload = function() {
-  if (lastPick !== null) {
-    changeTheme(lastPick);
-  }
-};
+if (lastPick !== null) {
+  changeTheme(lastPick);
+}
 
 function changeTheme(theme) {
   $("head link#theme").attr("href", "/theme-" + theme + ".css");
   $("header img").attr("src", logos[theme]);
-  $("td.new img").attr("src", newmoon[theme]);
+  $("td.new img").attr("src", newmoon[theme]); // newmoon is declared in script.js
 }
 
 // change on click
@@ -37,4 +28,40 @@ $(".pick").each(function() {
     changeTheme(theme);
     localStorage.setItem("themePick", theme);
   });
+});
+
+// hide page until page is fully loaded
+$("header, main, footer").hide();
+
+let bg, col;
+switch(lastPick) {
+  case "light":
+  default:
+    bg = "#E7EEFE";
+    break;
+  case "dark":
+    bg = "#031030";
+    break;
+  case "warm":
+    bg = "#FFEDDD";
+    break;
+}
+$("#load").css("background-color", bg);
+
+var deg = 2;
+let transform = function () {
+  $("#load img").css("transform", "rotate(" + deg + "deg)");
+  deg += 2;
+}
+var rotate = setInterval(transform, 20);
+
+$(document).ready(function () {
+  setTimeout(function() {
+    $("#load").css("opacity", "0");
+    $("header, main, footer").show();
+  }, 500);
+  setTimeout(function() {
+    $("#load").hide();
+    clearInterval(rotate);
+  }, 1000);
 });
